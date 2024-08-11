b6e51cd1d1186314c0fa0480d35c35b2e7bb87a1
sudarshnamv <sudarshnamv@gmail.com>
2024-08-11 19:36:58 +0530

    Fixed functions and updated main logic

--- client_test.py
+++ client_test.py
@@ -1,25 +1,16 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint(self):
+        quote = {'stock': 'ABC', 'top_bid': {'price': 120.48}, 'top_ask': {'price': 121.20}}
+        expected = ('ABC', 120.48, 121.20, (120.48 + 121.20) / 2)
+        self.assertEqual(getDataPoint(quote), expected)
 
+    def test_getRatio(self):
+        self.assertEqual(getRatio(120, 60), 2)
+        self.assertEqual(getRatio(120, 0), None)  # Check division by zero
 
-  """ ------------ Add more unit tests ------------ """
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
